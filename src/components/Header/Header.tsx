76f576f28ed3a39238ebe075dcb13c18ed9ef80f
persiandarik <persiandarik@gmail.com>
2026-01-31 18:53:24 +0330

    [UPDATE] enhance header layout and styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,31 +4,37 @@ import styles from "./Header.module.css";
 
 export default function Header(): ReactElement {
   return (
-      <header className={styles.header}>
-        <img className="logo" src="/assets/logos/Logo.png" alt="swiftlogixtestwebstorm logo"/>
-        <nav>
-          <ul>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">Pricing</a>
-            </li>
-            <li>
-              <a href="#">Track Shipment</a>
-            </li>
-            <li>
-              <a href="#">About Us</a>
-            </li>
-          </ul>
-        </nav>
-        <div className="actions">
-          <button>Login</button>
-          <button>Sing up</button>
-        </div>
-      </header>
+    <header className={styles.header}>
+      <img
+        className={styles.logo}
+        src="/assets/logos/Logo.png"
+        alt="Swiftlogix Logo"
+      />
+      <nav>
+        <ul>
+          <li>
+            <a href="#" className={styles.active}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+          <li>
+            <a href="#">Track Shipment</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.actions}>
+        <button>Log In</button>
+        <button>Sign Up</button>
+      </div>
+    </header>
   );
-};
+}
