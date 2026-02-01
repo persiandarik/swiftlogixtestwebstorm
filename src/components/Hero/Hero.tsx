0459e598aca7818fd67be95b8ed720d525f921df
persiandarik <persiandarik@gmail.com>
2026-02-01 16:29:20 +0330

    [ADD] hero image

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,16 +5,22 @@ import Button, {Variant} from "../Button/Button.tsx";
 
 export default function Hero(): ReactElement {
   return (
-    <div className={styles.hero}>
-      <h1>Effortless Logistics, Every Step of the Way.</h1>
-      <p>
-        Manage your shipments, track in real-time, and get the best rates—all
-        from one platform.
-      </p>
-      <div className={styles.actions}>
-        <Button>Get a Free Quote</Button>
-        <Button variant={Variant.OUTLINE}>Learn More</Button>
+      <div className={styles.hero}>
+        <div className={styles["background-image"]}>
+          <img src="/assets/images/hero.png" alt=""/>
+          <div className={styles.overlay}/>
+        </div>
+        <div className={styles.writings}>
+          <h1>Effortless Logistics, Every Step of the Way.</h1>
+          <p>
+            Manage your shipments, track in real-time, and get the best rates—all
+            from one platform.
+          </p>
+          <div className={styles.actions}>
+            <Button>Get a Free Quote</Button>
+            <Button variant={Variant.OUTLINE}>Learn More</Button>
+          </div>
+        </div>
       </div>
-    </div>
   );
 }
